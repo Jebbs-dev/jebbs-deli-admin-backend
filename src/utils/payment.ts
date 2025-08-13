2897bf432d02c45d2e6f21bef4638c3aa9bfe4e1
Jebbs-dev <ojebiyifulness@gmail.com>
2025-08-13 23:05:39 +0100

    feat: add paystack payment utils

---
+++ src/utils/payment.ts
@@ -0,0 +1,81 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+
+export const secretKey = process.env.PAYSTACK_SECRET_KEY;
+
+type AxiosMethod = (
+  url: string,
+  data?: any,
+  config?: AxiosRequestConfig
+) => Promise<AxiosResponse>;
+
+/**
+ * Generic fetch wrapper for Paystack
+ */
+const fetchPaystackResponse = async (
+  url: string,
+  method: AxiosMethod,
+  data?: Record<string, any>
+) => {
+  const config: AxiosRequestConfig = {
+    headers: {
+      Authorization: `Bearer ${secretKey}`,
+    },
+  };
+
+  const response = await method(url, data, config);
+  return response;
+};
+
+export const InitialiseTransaction = async (data: Record<string, any>) => {
+  const url = "https://api.paystack.co/transaction/initialize";
+
+  try {
+    const response = await fetchPaystackResponse(url, axios.post, data);
+
+    const { reference, access_code, authorization_url } = response.data.data;
+
+    return { reference, access_code, authorization_url };
+  } catch (error) {
+    console.error("Paystack error:", error);
+    throw error;
+  }
+};
+
+export const verifyTransaction = async (reference: string) => {
+  const url = `https://api.paystack.co/transaction/verify/${reference}`;
+
+  try {
+    const paystackResponse = await fetchPaystackResponse(url, axios.get);
+
+    return paystackResponse.data;
+  } catch (error) {
+    console.error("Paystack error:", error);
+    throw error;
+  }
+};
+
+export const listTransactions = async () => {
+  const url = "https://api.paystack.co/transaction";
+
+  try {
+    const paystackResponse = await fetchPaystackResponse(url, axios.get);
+
+    return paystackResponse.data;
+  } catch (error) {
+    console.error("Paystack error:", error);
+    throw error;
+  }
+};
+
+export const fetchTransactionById = async (id: number) => {
+  const url = `https://api.paystack.co/transaction/${id}`;
+
+  try {
+    const paystackResponse = await fetchPaystackResponse(url, axios.get);
+
+    return paystackResponse.data;
+  } catch (error) {
+    console.error("Paystack error:", error);
+    throw error;
+  }
+};
